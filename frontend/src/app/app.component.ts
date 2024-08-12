6fa221b16be5004514da7bea1665aca8cb947149
Akshat <akshat170902@gmail.com>
2024-08-12 15:31:01 +0530

    course added

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CourseListComponent } from "./component/course-list/course-list.component";
+import { CourseComponent } from "./component/course/course.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CourseListComponent],
+  imports: [RouterOutlet, CourseListComponent, CourseComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ frontend/src/app/component/course/course.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course',
+  standalone: true,
+  imports: [],
+  templateUrl: './course.component.html',
+  styleUrl: './course.component.css'
+})
+export class CourseComponent {
+
+}
