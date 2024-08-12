5183ff503ca52561c427ab06fb05ab438705d3a7
Akshat <akshat170902@gmail.com>
2024-08-12 13:49:48 +0530

    added pages

---
+++ frontend/src/app/page/signup-page/signup-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signup-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './signup-page.component.html',
+  styleUrl: './signup-page.component.css'
+})
+export class SignupPageComponent {
+
+}
