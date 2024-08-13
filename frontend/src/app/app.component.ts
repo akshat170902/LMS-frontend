49b82dde93b086f1df84eacfdb31070b5a6eff17
sakshi8299 <sakshm8299@gmail.com>
2024-08-13 21:27:05 +0530

    sign-in-page/sign-up-page: Added

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CourseListComponent } from "./component/course-list/course-list.component";
 import { CourseComponent } from "./component/course/course.component";
+import { LoginPageComponent } from './page/login-page/login-page.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { SignupPageComponent } from './page/signup-page/signup-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CourseListComponent, CourseComponent],
+  imports: [RouterOutlet, LoginPageComponent,SignupPageComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
