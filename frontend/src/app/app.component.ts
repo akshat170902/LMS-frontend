6c895cd2a3ff548a10088f13bf2c6a00e09241d9
Prakhar Prabudh <prakhar.prabudh@ukg.com>
2024-08-13 15:50:36 +0530

    sidebar-ui

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CourseListComponent } from "./component/course-list/course-list.component";
 import { CourseComponent } from "./component/course/course.component";
+import { SidebarComponent } from './component/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CourseListComponent, CourseComponent],
+  imports: [RouterOutlet, CourseListComponent, CourseComponent,SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
