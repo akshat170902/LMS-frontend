339c5e23e8bfff74a53dca58ab05955c24de44a6
Akshat <akshat170902@gmail.com>
2024-08-16 12:20:55 +0530

    fixed sidebarr :

--- frontend/src/app/page/course-page/course-page.component.ts
+++ frontend/src/app/page/course-page/course-page.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { CourseComponent } from "../../component/course/course.component";
+import { QueryComponent } from "../../component/query/query.component";
+import { NavbarComponent } from "../../component/navbar/navbar.component";
+import { SidebarComponent } from "../../component/sidebar/sidebar.component";
 
 @Component({
   selector: 'app-course-page',
   standalone: true,
-  imports: [],
+  imports: [CourseComponent, QueryComponent, NavbarComponent, SidebarComponent],
   templateUrl: './course-page.component.html',
   styleUrl: './course-page.component.css'
 })
