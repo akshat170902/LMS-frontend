73ad6da05a6c6b93153f60b2bdf77776e37fc4ad
vikasl <vikaslellapalli12@gmail.com>
2024-08-19 16:25:36 +0530

    courses all

--- frontend/src/app/page/all-courses-page/all-courses-page.component.ts
+++ frontend/src/app/page/all-courses-page/all-courses-page.component.ts
@@ -4,22 +4,27 @@ import { SidebarComponent } from '../../component/sidebar/sidebar.component';
 import { CommonModule } from '@angular/common';
 import { DataService } from '../../service/data.service';
 import { FooterComponent } from '../../component/footer/footer.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-all-courses-page',
   standalone: true,
-  imports: [NavbarComponent,SidebarComponent,CommonModule,FooterComponent],
+  imports: [NavbarComponent,SidebarComponent,CommonModule,FooterComponent,RouterModule],
   templateUrl: './all-courses-page.component.html',
   styleUrl: './all-courses-page.component.css'
 })
 export class AllCoursesPageComponent {
   courses: any[]=[];
+  ids: any={};
+  
   constructor(private dataServices: DataService){
     // Fetch all-courses data
     this.dataServices.getAllCourses().subscribe(response =>{
       this.courses=response;
       console.log(response);
     });
+
   }
+
 
 }
