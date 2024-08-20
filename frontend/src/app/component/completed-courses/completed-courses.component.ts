eafe8c8fb37e650ba42e7202ba9563fb3ae571e1
Akshat <akshat170902@gmail.com>
2024-08-20 15:06:27 +0530

    added the components

---
+++ frontend/src/app/component/completed-courses/completed-courses.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-completed-courses',
+  standalone: true,
+  imports: [],
+  templateUrl: './completed-courses.component.html',
+  styleUrl: './completed-courses.component.css'
+})
+export class CompletedCoursesComponent {
+
+}
---
+++ frontend/src/app/component/pending-courses/pending-courses.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pending-courses',
+  standalone: true,
+  imports: [],
+  templateUrl: './pending-courses.component.html',
+  styleUrl: './pending-courses.component.css'
+})
+export class PendingCoursesComponent {
+
+}
