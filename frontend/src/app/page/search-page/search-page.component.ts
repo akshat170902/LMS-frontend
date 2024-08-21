0e847456db8615a11ddfb11588bd3fb4b94cef6d
Akshat <akshat170902@gmail.com>
2024-08-21 20:08:46 +0530

    integration

--- frontend/src/app/page/search-page/search-page.component.ts
+++ frontend/src/app/page/search-page/search-page.component.ts
@@ -2,10 +2,12 @@ import { Component,OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { DataService } from '../../service/data.service';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from "../../component/navbar/navbar.component";
+import { SidebarComponent } from "../../component/sidebar/sidebar.component";
 @Component({
   selector: 'app-search-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NavbarComponent, SidebarComponent],
   templateUrl: './search-page.component.html',
   styleUrl: './search-page.component.css'
 })
